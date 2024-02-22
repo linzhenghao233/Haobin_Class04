d82b3182cf45aa18d752f17c1440bc1a6a48e981
linzhenghao233 <1392872316@qq.com>
2024-02-22 23:24:55 +0800

    0222

--- Haobin_Class04.c
+++ Haobin_Class04.c
@@ -11,15 +11,47 @@ typedef struct Stack {
 	pNode pBottom;
 }Stack, * pStack;
 
-void InitStack();
+void InitStack(pStack pS);
+void PushStack(pStack pS, int val);
+void TraverseStack(pStack pS);
 
 int main(void) {
 	Stack S;
 
-	InitStack();
-	PushStack();
-	PushStack();
-	TraverseStack();
+	InitStack(&S);
+	PushStack(&S, 1);
+	PushStack(&S, 2);
+	TraverseStack(&S);
 
 	return 0;
+}
+
+void InitStack(pStack pS) {
+	pS->pTop = (pNode)malloc(sizeof(Node));
+	if (pS->pTop == NULL) {
+		printf("¶¯Ì¬ÄÚ´æ·ÖÅäÊ§°Ü£¡\n");
+		exit(-1);
+	}
+	pS->pBottom = pS->pTop;
+	pS->pBottom->pNext = NULL;
+}
+
+void PushStack(pStack pS, int val) {
+	pS->pTop->Data = val;
+	pNode pNew = (pNode)malloc(sizeof(Node));
+	if (pNew == NULL) {
+		printf("¶¯Ì¬ÄÚ´æ·ÖÅäÊ§°Ü£¡\n");
+		exit(-1);
+	}
+	pNew->pNext = NULL;
+	pS->pTop->pNext = pNew;
+}
+
+void TraverseStack(pStack pS) {
+	pStack pTemp = pS;
+
+	while (pTemp->pBottom->pNext != NULL) {
+		printf("%d", pTemp->pBottom->pNext->Data);
+		pTemp->pBottom = pTemp->pBottom->pNext;
+	}
 }
